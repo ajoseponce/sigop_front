26bad6c31793bcfb34050bce5dcbfd585ea38c4b
jponce <albertojoseponce@gmail.com>
2026-09-04 17:40:09 -0300

    chore: actualizar version a 1.0.7

--- src/app/layout/shell/shell.component.ts
+++ src/app/layout/shell/shell.component.ts
@@ -77,7 +77,7 @@ export class ShellComponent implements OnInit {
 
   sidebarAbierto = signal(true);
   navItems = signal<NavItem[]>(DEFAULT_NAV_ITEMS);
-  appVersion = '1.0.6';
+  appVersion = '1.0.7';
 
   submenuAbierto = signal<string | null>(null);
 
